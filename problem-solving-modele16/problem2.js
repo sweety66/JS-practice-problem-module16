70f36837db6e1843e0232b0e053b13cf441204de
sweety66 <sweety41deb@gmail.com>
2022-08-08 17:57:37 +0600

    problem2 added

---
+++ problem-solving-modele16/problem2.js
@@ -0,0 +1,10 @@
+var mathematicsMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+var totalMarkes = mathematicsMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks
+console.log(totalMarkes);
+var avgMarkes = totalMarkes/5
+avgMarkes = avgMarkes.toFixed(2);
+console.log(avgMarkes);
